720c2b145c4e3c2db6f6cce7b33c746b89a5bb5d
Sufal Roy <sufalroy1997@gmail.com>
2024-09-10 16:38:41 +0530

    Add cors filter in security config

--- beanhub-be/src/main/java/org/skytel/beanhub/security/SecurityConfig.java
+++ beanhub-be/src/main/java/org/skytel/beanhub/security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
                 )
